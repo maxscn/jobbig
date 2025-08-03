ae102eb5f5c1a265c0b2befc6298bae10382a4bc
Maximilian Schön <maximilian.ak.schon@gmail.com>
2025-08-03 14:38:08 +0200

    adding vercel preset

--- apps/studio/app.config.ts
+++ apps/studio/app.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 import tanstackRouter from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
+	server: {
+		preset: "vercel",
+	},
 	vite: {
 		plugins: [tanstackRouter({ target: "solid" }), tailwindcss()],
 		resolve: {
