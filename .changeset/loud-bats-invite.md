6822c951449e077ca56d90d53ee23f8f261433e6
Maximilian Schön <maximilian.ak.schon@gmail.com>
2025-08-06 17:14:21 +0200

    export plugins

---
+++ .changeset/loud-bats-invite.md
@@ -0,0 +1,5 @@
+---
+"@jobbig/core": patch
+---
+
+Export plugins
--- packages/core/src/plugins/index.ts
+++ packages/core/src/plugins/index.ts
@@ -1,3 +1,5 @@
 export * from "./cron-plugin";
 export * from "./event-plugin";
+export * from "./nextjs-plugin";
 export * from "./server-plugin";
+export * from "./sqs-plugin";
